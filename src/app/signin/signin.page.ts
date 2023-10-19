2ff97aad8c83c6ae9ce59c42d0be0eaed564f41a
Zeonsi <tomasgarrido0212@gmial.com>
2023-10-19 16:31:12 -0300

    validacion con base de datos

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { DjangoService } from '../service/django.service';
-
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -11,25 +10,42 @@ import { DjangoService } from '../service/django.service';
 })
 export class SigninPage implements OnInit {
   username: string = ''; 
-
-  constructor(private router: Router, private djangoApi: DjangoService) {
+  users: any;
+  forma!: FormGroup;
+  passw: string ='';
+  constructor(private router: Router, private djangoApi: DjangoService, private fb:FormBuilder) {    
     this.djangoApi.getUsuarios().subscribe(
       (usuarios)=>{
         console.log(usuarios);
+        this.users = usuarios;
+        console.log(this.users);
       }
       ,
       (error)=>{
         console.log(error);
       }
     );
+    this.crearFormulario()
+  }
+
+  crearFormulario(){
+    this.forma = this.fb.group({
+      usuario:['', [Validators.required, Validators.minLength(5)]],
+      pass:['', [Validators.required]]  
+    })
   }
 
   ngOnInit() {
   }
   
-  goTo() {
-    this.router.navigate(['/home'], {
-      state: {user: this.username},
-    })
+  goTo(users) {
+    for (let index = 0; index < users.length; index++) {     
+      if (users[index].nombre_usuario == this.username && users[index].password_usuario == this.passw){
+        this.router.navigate(['/home'], {
+          state: {user: users[index].direccion_usuario},
+        })
+      }
+      
+    }
   }
 }
