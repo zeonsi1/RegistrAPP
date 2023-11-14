ad2465f41a7f567d33feb0f3b6f2277fc5b38b3b
Zeonsi <tomasgarrido0212@gmial.com>
2023-11-14 20:36:24 -0300

    solucion errores

--- src/app/loader/loader.page.ts
+++ src/app/loader/loader.page.ts
@@ -7,7 +7,8 @@ import { Router } from '@angular/router';
 })
 export class LoaderPage implements OnInit {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
     setTimeout(() => {
